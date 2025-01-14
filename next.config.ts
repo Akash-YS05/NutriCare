728d49edd758fee8afae444ee0717be44f4b4767
Akash-YS05 <akash.pan05@gmail.com>
2025-01-14 22:35:00 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,42 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['plus.unsplash.com', 'img.freepik.com', 'cdn.dribbble.com', 'i.pinimg.com'], // Add this domain for Unsplash premium images
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**', // Allow all paths from this domain
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**', // Allow all paths from this domain
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dribbble.com',
+        port: '',
+        pathname: '/**', // Allow all paths from this domain
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**', // Allow all paths from this domain
+      },
       {
         protocol: 'https',
         hostname: 'v0.blob.com',
         port: '',
-        pathname: '/**',
+        pathname: '/**', // Allow all paths from this domain
       },
       {
         protocol: 'https',
         hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
         port: '',
-        pathname: '/**',
+        pathname: '/**', // Allow all paths from this domain
       },
     ],
   },
